2c995001a902fc5ce5aa3c498b30e95d5e378a94
Steve Ojeda <smojeda7190@gmail.com>
2020-01-11 17:05:16 -0800

    set up counter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import CountDisplay from './Components/CountDisplay'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          hey
-      </header>
-    </div>
-  );
+const [count, setCount] = useState(0);
+
+const increment = () => {
+  setCount(count+1)
+  console.log(count);
+}
+
+const decrement = () => {
+  setCount(count-1)
+  console.log(count);
+}
+
+useEffect( () => {console.log('count changed')},[count])
+
+return (
+  <div className="App">
+    <header className="App-header">
+      <CountDisplay count={count}/>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </header>
+  </div>
+);
 }
 
 export default App;
